6bd25f43624071e6022bb077b2f2ccb56445b462
danrleywillyan <danrleywillian@gmail.com>
2016-08-17 22:29:28 -0300

    add Movie.hpp in Movie.cpp and some appear.

--- SourcesCodesExercicios/C++/Movies/Movie.hpp
+++ SourcesCodesExercicios/C++/Movies/Movie.hpp
@@ -1,4 +1,47 @@
-#ifndef MOVIE
-#define MOVIE
+#ifndef MOVIE_H
+#define MOVIE_H
+
+using namespace std;
+
+//classe filme
+class Movie
+{
+  public:
+    Movie(string title = "default", int year = 2016, string directorName = "unknown", int numberOfStars = 1){
+      this->title = title;
+      this->year = year;
+      this->directorName = directorName;
+      this->numberOfStars = numberOfStars;
+    }
+    string getTitle()
+    {
+      return this->title;
+    }
+    void setTitle(string title)
+    {
+      this->title = title;
+    }
+    int getYear()
+    {
+      return this->year;
+    }
+    string getDirector()
+    {
+      return this->directorName;
+    }
+    int getStars()
+    {
+      return this->numberOfStars;
+    }
+    // Movie(string str)
+    // {
+    //   this->title = str;
+    // }
+  private:
+    string title;
+    int year;
+    string directorName;
+    int numberOfStars;
+};
 
 #endif
--- SourcesCodesExercicios/C++/Movies/main.cpp
+++ SourcesCodesExercicios/C++/Movies/main.cpp
@@ -1,52 +1,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Movie.hpp"
 #include <thread>
 #include <cstdlib>
+#include "Movie.hpp"
 
 
 using namespace std;
-//classe filme
-class Movie
-{
-  public:
-    Movie(string title = "default", int year = 2016, string directorName = "unknown", int numberOfStars = 1){
-      this->title = title;
-      this->year = year;
-      this->directorName = directorName;
-      this->numberOfStars = numberOfStars;
-    }
-    string getTitle()
-    {
-      return this->title;
-    }
-    void setTitle(string title)
-    {
-      this->title = title;
-    }
-    int getYear()
-    {
-      return this->year;
-    }
-    string getDirector()
-    {
-      return this->directorName;
-    }
-    int getStars()
-    {
-      return this->numberOfStars;
-    }
-    // Movie(string str)
-    // {
-    //   this->title = str;
-    // }
-  private:
-    string title;
-    int year;
-    string directorName;
-    int numberOfStars;
-};
 
 Movie Add()
 {
